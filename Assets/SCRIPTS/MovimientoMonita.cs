d9f46c0ac1d476690141727802580b2467ae46fb
Lizzy95 <lizzi16.gd95@gmail.com>
2016-02-20 20:14:55 -0600

    movimientomonita/puerta

--- Assets/SCRIPTS/MovimientoMonita.cs
+++ Assets/SCRIPTS/MovimientoMonita.cs
@@ -5,6 +5,7 @@ public class MovimientoMonita : MonoBehaviour {
 
 	public float speed = 3.5f;
 	public bool bDerecha = false;
+	public int jumpSpeed = 30;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,7 @@ public class MovimientoMonita : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKey(KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			if (bDerecha == false) {
 				Vector3 theScale = transform.localScale;
 				theScale.x *= -1;
@@ -22,16 +23,21 @@ public class MovimientoMonita : MonoBehaviour {
 				bDerecha = true;
 			}
 
-			transform.position += Vector3.left *speed * Time.deltaTime;
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			if (bDerecha == true) {
 				Vector3 theScale = transform.localScale;
 				theScale.x *= -1;
 				transform.localScale = theScale;
 				bDerecha = false;
 			}
-			transform.position += Vector3.right * speed* Time.deltaTime;
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		if (Input.GetKey (KeyCode.Space)) {
+			Vector3 theScale = transform.position;
+			transform.position += Vector3.up * jumpSpeed * Time.deltaTime;
+		}
+
 	}
 }
