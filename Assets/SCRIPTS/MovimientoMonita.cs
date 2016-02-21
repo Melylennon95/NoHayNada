3e2bdae93831bd18caceff03641eb7b2060ecf72
Lizzy95 <lizzi16.gd95@gmail.com>
2016-02-20 19:21:40 -0600

    assest

---
+++ Assets/SCRIPTS/MovimientoMonita.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoMonita : MonoBehaviour {
+
+	public float speed = 3.5f;
+	public bool bDerecha = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (bDerecha == false) {
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+				bDerecha = true;
+			}
+
+			transform.position += Vector3.left *speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if (bDerecha == true) {
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+				bDerecha = false;
+			}
+			transform.position += Vector3.right * speed* Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/SCRIPTS/MovimientoPuerta.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoPuerta : MonoBehaviour {
+	public float timer;
+	public float speed = 3.5f;
+	public Vector3 randomplace;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Mathf.PingPong(Time.time, 8)-5, transform.position.y, transform.position.z);
+	}
+}
